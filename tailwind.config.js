7b166cfe81999aea618979359d51d76539526173
Akriti Ghosh <akritighosh98@gmail.com>
2024-08-18 22:49:43 +0530

    new colors added for tailwind classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      navy: "#2f3369",
+      brown: "#5c4e4e",
+      teal: "#0097a7",
+      coral: "#ff9e7a",
+      dgray: "#747474",
+      beige: "#e3dddc",
+    },
     extend: {},
   },
   plugins: [],
