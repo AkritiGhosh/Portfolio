9c20f04165a745aa7d5438317671d75ca1149d55
Akriti Ghosh <akritighosh98@gmail.com>
2024-08-19 15:21:47 +0530

    tailwind colors - extend not replace - fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      navy: "#2f3369",
-      brown: "#5c4e4e",
-      teal: "#0097a7",
-      coral: "#ff9e7a",
-      dgray: "#747474",
-      beige: "#e3dddc",
+    extend: {
+      colors: {
+        navy: "#2f3369",
+        brown: "#5c4e4e",
+        teal: "#0097a7",
+        coral: "#ff9e7a",
+        dgray: "#747474",
+        beige: "#e3dddc",
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
