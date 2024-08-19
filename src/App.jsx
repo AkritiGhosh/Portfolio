1088528cb785e362c1bec6a4a713c35e97f35a3c
Akriti Ghosh <akritighosh98@gmail.com>
2024-08-19 15:22:48 +0530

    section scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import AboutMe from "./sections/AboutMe";
+import Landing from "./sections/Landing";
+
+export const smoothScroll = (e, sectionId) => {
+  let section = document.getElementById(sectionId);
+  e.preventDefault(); // Stop Page Reloading
+  section && section.scrollIntoView({ behavior: "smooth", block: "start" });
+};
 
 function App() {
-
   return (
-    <></>
-  )
+    <main className="w-screen h-screen overflow-hidden bg-navy p-1 !scroll-smooth">
+      <div className="w-full h-full snap-y snap-mandatory overflow-auto">
+        <Landing />
+        <AboutMe />
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
