2532ce03b847b3bf11070062b76103b257f77fc3
Akriti Ghosh <akritighosh98@gmail.com>
2024-08-19 15:22:33 +0530

    Create AboutMe.jsx

---
+++ src/sections/AboutMe.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AboutMe = () => {
+  return (
+      <div id="about" className="w-full h-full rounded-xl md:px-[10%] px-4 snap-start snap-always">
+          <h1 className=""></h1>
+    </div>
+  );
+};
+
+export default AboutMe;
