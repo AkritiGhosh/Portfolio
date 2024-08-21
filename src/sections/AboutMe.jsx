e989cf8c3cc3da67de75dc6a9621b5b86737ce81
Akriti Ghosh <akritighosh98@gmail.com>
2024-08-21 22:07:22 +0530

    about me content

--- src/sections/AboutMe.jsx
+++ src/sections/AboutMe.jsx
@@ -14,20 +14,30 @@ const AboutMe = () => {
     <div
       ref={aboutRef}
       id="about"
-      className="md:w-3/5 w-full h-full rounded-xl md:px-[10%] px-4 snap-start snap-always text-right float-right"
+      className="md:w-9/12 w-full h-full rounded-xl md:px-[10%] px-4 snap-start snap-always text-right float-right"
     >
       {/* <h1 className=""></h1> */}
       <Typewriter>
-        <div
-          className="text-xl text-white font-medium"
-        >
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          finibus mollis commodo. Maecenas tristique viverra suscipit. Fusce
-          luctus risus ac nulla feugiat molestie. Nunc rhoncus ullamcorper
-          sodales. Morbi faucibus aliquet magna quis posuere. Aenean condimentum
-          orci et commodo facilisis. Sed tincidunt justo at ante fringilla
-          faucibus. Mauris ut laoreet felis, ac efficitur eros. Fusce id egestas
-          magna, vulputate dapibus lacus.
+        <div className="text-xl text-white font-normal leading-relaxed">
+          I am a Software Engineer currently living & working in Bhilai, India.
+          As a CS major, I love to get my hands dirty from challenging & unique
+          problems. While I was a student, I worked remotely on different data
+          analysis internships and freelanced as a Deep Learning Engineer for a
+          Nevada-based company.
+          <br />
+          Due to the pandemic, I was working remotely, as a Software Engineer at
+          Techment, with gracious and welcoming colleagues. This experience gave
+          me the confidence of working on production-scale projects. (Read
+          more).
+          <br />
+          After over 2 years in Techment, I joined my current company SproutsAI,
+          as a Senior Software Engineer. I was incharge of leading the frontend
+          team. (Read more)
+          <br />
+          In my free time, I like to listen to bollywood music and read novels.
+          I love to travel to different places and enjoy cultural and scenic
+          diversity in India, though my favorite remains beaches. I hope to
+          travel more and perhaps get an opportunity to travel for work.
         </div>
       </Typewriter>
     </div>
