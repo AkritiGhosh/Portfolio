39bb2f77c4be0c65525c1cc2d18763b2617cab12
Akriti Ghosh <akritighosh98@gmail.com>
2024-08-21 22:07:41 +0530

    new sections created

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Experience = () => {
   return (
-    <div>Experience</div>
-  )
+    <div className="relative w-full h-full rounded-xl bg-white md:px-[10%] px-4 pb-6 snap-start snap-always flex flex-col text-navy justify-center gap-4"></div>
+  );
 }
 
 export default Experience
---
+++ src/sections/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+      <div className="relative w-full h-1/4 bg-navy  snap-end snap-always">
+          <div className="bg-beige w-1/3 h-10 absolute -top-5 outline outline-4 outline-beige border-brown border-2 border-dashed m-1"></div>
+    </div>
+  );
+};
+
+export default Footer;
